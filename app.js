568e4b1186efee52d225edebe66a9eed9d2058bb
Subhajit Kundu <kundusubhajit73@gmail.com>
2024-10-13 23:29:50 +0530

    cors option chaged

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ if (process.env.NODE_ENV === 'dev') {
 const corsOption = {
   //it is not allowed to pass any type of cookie or authentication header when origin sets to '*" you have to be specified"
   origin: [
+    'http://157.173.222.205:5173',
+    'http://157.173.222.205:5175',
+    'http://157.173.222.205:5174',
     'http://localhost:5173',
     'https://quaintprint-main.vercel.app',
     'https://quaintprint-artist.vercel.app',
